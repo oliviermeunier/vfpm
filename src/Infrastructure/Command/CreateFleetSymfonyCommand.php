1d8d29ce96e6dc8f04e7fa186a931c525f7961ac
Olivier Meunier <oli.meunier@gmail.com>
2023-07-05 17:44:24 +0200

    [Step 2] CreateFleet Symfony Command with InMemory Repository

---
+++ src/Infrastructure/Command/CreateFleetSymfonyCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Fulll\Infrastructure\Command;
+
+use Fulll\Application\COmmand\CreateFleet\CreateFleetCommand;
+use Fulll\Application\COmmand\CreateFleet\CreateFleetCommandHandler;
+use Fulll\Infrastructure\Repository\InMemory\InMemoryFleetRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class CreateFleetSymfonyCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('create')
+            ->setDescription('Cretae a brand new fleet')
+            ->setHelp('php bin/fleet create <user-id>')
+            ->addArgument('user-id', InputArgument::REQUIRED, 'User Id');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userId = $input->getArgument('user-id');
+        $command = new CreateFleetCommand($userId);
+        $handler = new CreateFleetCommandHandler(new InMemoryFleetRepository());
+        $fleetId = $handler($command);
+
+        $output->writeln(sprintf('Fleet created successfully with ID %s', $fleetId));
+        return Command::SUCCESS;
+    }
+}
