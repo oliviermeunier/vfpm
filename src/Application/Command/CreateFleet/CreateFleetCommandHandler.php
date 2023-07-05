9dd776e8a8e67e0903ab9ff21bb1b6860da7b099
Olivier Meunier <oli.meunier@gmail.com>
2023-07-05 11:51:36 +0200

    [Step 2] CreateFleetCommandHandler (InMemory Repository)

--- src/Application/Command/CreateFleet/CreateFleetCommandHandler.php
+++ src/Application/Command/CreateFleet/CreateFleetCommandHandler.php
@@ -2,10 +2,25 @@
 
 namespace Fulll\Application\COmmand\CreateFleet;
 
+use Fulll\Domain\Interface\FleetRepositoryInterface;
+use Fulll\Domain\Model\Fleet;
+use Fulll\Domain\Model\FleetId;
+
 class CreateFleetCommandHandler {
     
-    // public function __invoke(CreateFleetCommand $command): string
-    // {
-    //     $userId = $command->getUserId();        
-    // }
+    public function __construct(
+        private FleetRepositoryInterface $fleetRepository
+    )
+    {
+        
+    }
+
+    public function __invoke(CreateFleetCommand $command): FleetId
+    {
+        $userId = $command->getUserId();        
+        $fleet = new Fleet($userId);
+        $fleetId = $this->fleetRepository->persist($fleet);
+
+        return $fleetId;
+    }
 }
