5837ff1999fce721e6cbd833fa603d202c76f77e
Olivier Meunier <oli.meunier@gmail.com>
2023-07-05 11:53:30 +0200

    [Step 2] VehicleRepository persist returns VehicleId

--- src/Domain/Interface/VehicleRepositoryInterface.php
+++ src/Domain/Interface/VehicleRepositoryInterface.php
@@ -4,9 +4,10 @@ namespace Fulll\Domain\Interface;
 
 use Fulll\Domain\Model\PlateNumber;
 use Fulll\Domain\Model\Vehicle;
+use Fulll\Domain\Model\VehicleId;
 
 interface VehicleRepositoryInterface
 {
-    public function persist(Vehicle $vehicle): string;
+    public function persist(Vehicle $vehicle): VehicleId;
     public function findByPlateNumber(string|PlateNumber $plateNumber): ?Vehicle;
 }
---
+++ src/Infrastructure/Repository/InMemory/InMemoryVehicleRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Fulll\Infrastructure\Repository;
+
+use Fulll\Domain\Model\Vehicle;
+use Fulll\Domain\Model\PlateNumber;
+use Fulll\Domain\Interface\VehicleRepositoryInterface;
+use Fulll\Domain\Model\VehicleId;
+use Fulll\Domain\Shared\ValueObject\UuidV4Generator;
+
+class InMemoryVehicleRepository implements VehicleRepositoryInterface
+{
+    private static array $vehicleCollection;
+
+    public function __construct()
+    {
+        static::$vehicleCollection = [];
+    }
+
+    public function persist(Vehicle $vehicle): VehicleId
+    {
+        $vehicle->setId(new VehicleId(UuidV4Generator::generate()));
+
+        static::$vehicleCollection[] = $vehicle;
+
+        return $vehicle->getId();
+    }
+
+    public function findByPlateNumber(string|PlateNumber $plateNumber): ?Vehicle
+    {
+        if (is_string($plateNumber)) {
+            $plateNumber = new PlateNumber($plateNumber);
+        }
+
+        foreach (static::$vehicleCollection as $vehicle) {
+            if ($vehicle->getPlateNumber()->equals($plateNumber)) {
+                return $vehicle;
+            }
+        }
+        return null;
+    }
+}
